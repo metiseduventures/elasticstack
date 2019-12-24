ea47be343409be861a90328c16243226f188708a
amitadda <amit.pundir@adda247.com>
2019-12-24 11:22:00 +0530

    scripts for scale up/down qa1 env/dbs

---
+++ qa1db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+arg1="$1";
+
+case "$arg1" in
+	scaleup)
+		dbaction="start-db-instance";;
+	scaledown)
+		dbaction="stop-db-instance";;
+	*)
+		echo  "usage: qa1db.sh (scaleup/scaledown)"
+		exit 1;
+esac
+
+
+qa1db="storefront-analytics-qa1db bigservicedb-qa1db coupon-qa1db video-streaming-qa1db user-qa1db timeline-qa1db storefront-qa1db rankanalytics-qa1db pushservicedb-qa1db erp-qa1 extradb-qa1db";
+
+for i in $qa1db
+do
+        echo "Stopping database  $i";
+	aws rds $dbaction --db-instance-identifier $i;
+done

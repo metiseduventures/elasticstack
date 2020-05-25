73e54d648ae359ba5c47a386e3c02f495028ae3b
amitadda <amit.pundir@adda247.com>
2020-05-25 22:15:16 +0530

    added usage and env list

--- getEnvIP.sh
+++ getEnvIP.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
 
+env=$1;
 
-env=storefrontuserprod
-aws elasticbeanstalk describe-environment-resources --environment-name $env --output=text | grep INSTANCES | cut -f2 | xargs -n 1 aws ec2 describe-instances --instance-ids --query 'Reservations[*].Instances[*].PublicIpAddress' --output=text
+if [ -z "$env" ];then
+	echo "usage : getEnvIP.sh <environment name>";
+	echo "List of available environments: ";
+	aws elasticbeanstalk describe-environments --query 'Environments[*].EnvironmentName' --output=text
+else
+	aws elasticbeanstalk describe-environment-resources --environment-name $env --output=text | grep INSTANCES | cut -f2 | xargs -n 1 aws ec2 describe-instances --instance-ids --query 'Reservations[*].Instances[*].PublicIpAddress' --output=text
+
+fi

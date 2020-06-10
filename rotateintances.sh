5391320d10fa0f03c97351ae0ae8669267cc1451
amitadda <amit.pundir@adda247.com>
2020-06-10 14:21:22 +0530

    script to rotate env instances for memory issues

---
+++ rotateintances.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+envlist="socialclientprod bigserviceproduction storefrontuserprod StorefrontAdminProduction testseriesprod storefrontuserprodadded"
+
+
+for env in $envlist
+do
+	date;
+	echo "Rotating instances for Environment :"$env;
+	ver="$(aws elasticbeanstalk describe-environments --environment-names $env --output=text | awk -F $'\t' '{ print $17}')"
+	aws elasticbeanstalk update-environment --environment-name $env --version-label $ver
+done

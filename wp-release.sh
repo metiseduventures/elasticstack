4a607ac735eb6bd775af26fa7876dd12a222b6e8
amitadda <amit.pundir@adda247.com>
2021-01-18 17:21:50 +0530

    minor changes

--- wp-release.sh
+++ wp-release.sh
@@ -11,15 +11,16 @@ do
 	response="$(/usr/local/bin/aws autoscaling describe-instance-refreshes --profile=prod --auto-scaling-group-name wpsites-asg --output=text --query 'InstanceRefreshes[0].Status' --output=text)";
 	if [ "$response" = "InProgress" ]; then
 		/usr/local/bin/aws autoscaling describe-instance-refreshes --profile=prod --auto-scaling-group-name wpsites-asg --query 'InstanceRefreshes[0].[Status,PercentageComplete,StatusReason]' --output=text;
-		sleep 10;
+		sleep 30;
 	elif [ "$response" = "Pending" ]; then
 		echo "Request is in Pending State";
-		sleep 10;
+		sleep 30;
 	elif [ "$response" = "Cancelling" ]; then
 		echo "Request is Cancelling";
-		sleep 10;
+		sleep 30;
 	elif [ "$response" = "Successful" ]; then
-		groupmsg="Deployment Completed Successfully";
+		groupmsg="Deployment on Production Wordpress Blogs Completed Successfully";
+		curl -X POST -H 'Content-type: application/json' --data '{"text":"'"${groupmsg}"'"}' https://hooks.slack.com/services/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;
 		exit 0;
 	elif [ "$response" = "Cancelled" ]; then
 		groupmsg="Nothing to do here , Last Deployment Cancelled";
